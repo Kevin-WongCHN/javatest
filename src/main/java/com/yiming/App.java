9e0e6d9606b1fa73c25caa4f388795d0a2c4406a
kevin_yiming <kevin_yimin@163.com>
2024-07-14 10:31:28 +0800

    hello

---
+++ src/main/java/com/yiming/App.java
@@ -0,0 +1,17 @@
+package com.yiming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int test(int a){
+        a++;
+        return a;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/yiming/AppTest.java
@@ -0,0 +1,20 @@
+package com.yiming;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+
+        int c = App.test(2);
+        assertTrue(c == 3);
+    }
+}
